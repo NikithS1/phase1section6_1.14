b1d5b10446aad186148dec0a12d8b95bc6759433
Nikiths1 <nikith.s1@mphasis.com>
2023-07-25 12:28:09 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace studentsdatadisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\\mphasis\\Day 12\\studentsdatadisplay\\studentsdatadisplay\\students.txt\"; // Replace "students.txt" with your actual text file name if different
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                Console.WriteLine("Student Data:");
+                Console.WriteLine("-------------");
+
+                // Skip the first line (header) and display student data line by line
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] data = lines[i].Split(',');
+                    int studentID = int.Parse(data[0]);
+                    string name = data[1];
+                    int grade = int.Parse(data[2]);
+
+                    Console.WriteLine($"ID: {studentID}, Name: {name}, Grade: {grade}");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"File not found: {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
